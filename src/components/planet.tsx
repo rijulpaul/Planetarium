e51e522089f56dc35ce0b34629317b95bf702105
0-Frostbite <neutral.zfab4@proton.me>
2025-10-07 23:24:18 +0530

    planet revolution

--- src/components/planet.tsx
+++ src/components/planet.tsx
@@ -5,12 +5,23 @@ import { TextureLoader } from "three"
 export default function Planet({name,props}) {
     const texture = useLoader(TextureLoader,`/textures/${name.toLowerCase()}.jpg`)
     const meshRef = useRef<THREE.Mesh>(null!)
+    const angleRef = useRef<number>(0)
 
     useFrame((_, delta) => {
+        // rotation
         if (props.rotation != 0) {
             const rotationSpeed = (2 * Math.PI) / (props.rotation * 3600);
-            meshRef.current.rotation.y += rotationSpeed * delta;
+            meshRef.current.rotation.y += rotationSpeed * delta * 1000;
         } 
+
+        // revolution
+        if (props.orbitalVel != 0) {
+            const omega = props.orbitalVel / props.distance * 0.00005;
+            angleRef.current += omega * delta * (1/(0.00005*0.00005));
+            const x = Math.cos(angleRef.current) * props.distance * 0.00005;
+            const z = Math.sin(angleRef.current) * props.distance * 0.00005;
+            meshRef.current.position.set(x,0,z)
+        }
     }) 
 
     return (
--- src/layouts/space/space.tsx
+++ src/layouts/space/space.tsx
@@ -11,8 +11,8 @@ export default function Space() {
         <div className="space">
             <Canvas camera={{ near: 0.1, far: 999999999, position: [600,0,0], fov: 30 }}>
                 <Environment background files="../../../public/space_spheremap_8k.jpg"/>
-                <ambientLight intensity={1.25}/>
-                <pointLight position={[0,0,0]} decay={0.9} intensity={300} color={"#ffccaa"}/>
+                <ambientLight intensity={0.65}/>
+                <pointLight position={[0,0,0]} decay={0.1} intensity={4} color={"#ffccaa"}/>
                 { Object.entries(Planets).map(([name,props])=><Planet name={name} props={props} />) }
                 <OrbitControls zoomSpeed={0.5} rotateSpeed={0.3} panSpeed={2}/>
                 <Stats/>
